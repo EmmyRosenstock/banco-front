d63828fd22b0e928848b567019c6e3bed434aa59
lab10maql41 <lab10maql41@debian-BULLSEYE-live-builder-AMD64>
2023-09-09 15:18:22 -0300

    first commit

---
+++ sistema_bancario/src/app/app-routing.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ListagemClientesComponent } from './pages/clientes/listagem-clientes/listagem-clientes.component';
+
+const routes: Routes = [
+  {
+    path:'',
+    component: ListagemClientesComponent,
+  },
+  {
+    path:'clientes',
+    component: ListagemClientesComponent,
+    children:[
+      {
+        path:'listagem',
+        component: ListagemClientesComponent,
+      },
+      {
+        path:'cadastro',
+        component: ListagemClientesComponent,
+      },
+      {
+        path:'editar/:id',
+        component: ListagemClientesComponent,
+      }
+      
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ sistema_bancario/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MaterialModule } from 'src/material/material.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule,
+    FormsModule,
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
